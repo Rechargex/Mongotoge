3ab713bea5037d6c0a9596c3b0435075d348a2a5
Rechargex <syhntax@gmail.com>
2023-01-12 15:25:43 +0800

    DB Connected

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose'); //1
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  //1
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose //1
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connections succesful!');
+  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
