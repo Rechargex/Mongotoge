43883e85fddd516f93d245c04359f97f3a89aa97
Rechargex <syhntax@gmail.com>
2023-01-12 16:20:04 +0800

    before MVC

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose //1
-  .connect(DB, {
+  .connect(process.env.DATABASE_LOCAL, {
+    // .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -19,8 +20,39 @@ mongoose //1
     console.log('DB connections succesful!');
   });
 
-// console.log(app.get('env'));
 // console.log(process.env);
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a name'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥', err);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
