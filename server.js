a54e6af183e56180dc88a7aeefe0be020e82bad6
Rechargex <syhntax@gmail.com>
2023-01-12 18:27:23 +0800

    before deleteTour

--- server.js
+++ server.js
@@ -1,57 +1,21 @@
-const mongoose = require('mongoose'); //1
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  //1
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-mongoose //1
-  .connect(process.env.DATABASE_LOCAL, {
-    // .connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((con) => {
     console.log(con.connections);
     console.log('DB connections succesful!');
-  });
-
-// console.log(process.env);
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a name'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥', err);
   });
 
 const port = process.env.PORT || 8000;
