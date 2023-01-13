4780ed79e352c34b17033e81d9e77eeb20f39a05
Rechargex <syhntax@gmail.com>
2023-01-13 12:11:02 +0800

    filtering

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -2,8 +2,40 @@ const Tour = require('./../Models/tourModels');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
-    console.log(req.requestTime);
+    console.log(req.query);
+
+    //BUILD A QUERY
+    //FILTERING
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //Advance Filtering
+    let queryStr = JSON.stringify(queryObj); // {difficulty: 'easy', duration:'{gte:5}} ---> {"difficulty":"easy","duration":{"gte":"5"}}
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); //gte,gt,lte,lt
+
+    let query = Tour.find(JSON.parse(queryStr)); //{"difficulty":"easy","duration":{"gte":"5"},"price":{"lt":"15000"}}
+
+    //SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      //sort('price ratingsAverage')
+    } else {
+      query = query.sort('createdAt');
+    }
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //SEND RESPONSE
+
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
@@ -39,7 +71,7 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    //const newTOurs = newTour({})
+    //const newTours = newTour({})
     //newTours.save()
     const newTour = await Tour.create(req.body);
     res.status(201).json({
@@ -74,10 +106,11 @@ exports.updateTour = async (req, res) => {
     });
   }
 };
+
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findOneAndDelete(req.id, req.body);
-    res.status(204).json({
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
       status: 'success',
       data: null,
     });
@@ -88,3 +121,20 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+/* exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.deleteOne({
+      _id: req.params.id,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+}; */
